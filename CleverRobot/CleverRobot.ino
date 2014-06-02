ffe58f0c0fc30d97aa2b3e77dd55e99511f2b5cc
astulnikov <shamm.sb@gmail.com>
2014-06-02 17:49:02 +0300

    Ultrasonic distance sensor added

--- CleverRobot/CleverRobot.ino
+++ CleverRobot/CleverRobot.ino
@@ -4,20 +4,27 @@
 #include "ADXL345.h"
 #include "HMC5883L.h"
 #include "ITG3200.h"
+#include "Ultrasonic.h" 
 
 const long ONE_MINUTE = 60000;
 
-HMC5883L compass;
-ITG3200 gyro;
-ADXL345 accel;
+//distance sensor
+const int TRIG_PIN = 54;
+const int ECHO_PIN = 55;
+
+Ultrasonic mFrontUltrasonic(TRIG_PIN, ECHO_PIN);
+
+HMC5883L mCompass;
+ITG3200 mGyro;
+ADXL345 mAccel;
 
 int16_t gx, gy, gz;
 int16_t ax, ay, az;
 
-volatile int ticksLeft;
-volatile int ticksRight;
-long timeLeft;
-long timeRight;
+volatile int mTicksLeft;
+volatile int mTicksRight;
+long mTimeLeft;
+long mTimeRight;
 
 void setup() {
   // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -28,18 +35,18 @@ void setup() {
 
   // initialize device
   Serial.println("Initializing I2C devices....");
-  accel.initialize();
-  gyro.initialize();
+  mAccel.initialize();
+  mGyro.initialize();
 
-  compass = HMC5883L(); //new instance of HMC5883L library
+  mCompass = HMC5883L(); //new instance of HMC5883L library
   setupHMC5883L(); //setup the HMC5883L
 
     // verify connection
   Serial.println("Testing device connections...");
-  Serial.println(accel.testConnection() ? "ADXL345 connection successful" : "ADXL345 connection failed");
+  Serial.println(mAccel.testConnection() ? "ADXL345 connection successful" : "ADXL345 connection failed");
 
   Serial.println("Testing device connections...");
-  Serial.println(gyro.testConnection() ? "HMC5883L connection successful" : "HMC5883L connection failed");
+  Serial.println(mGyro.testConnection() ? "HMC5883L connection successful" : "HMC5883L connection failed");
 
   //Digital Pin 2 Set As An Interrupt
   Serial.println("Attaching Interrupts for speed measure...");
@@ -53,9 +60,13 @@ void loop() {
   Serial.println(heading);
 
   Serial.print("RPM left: ");
-  Serial.println(getRPM(&ticksLeft, &timeLeft), DEC);
+  Serial.println(getRPM(&mTicksLeft, &mTimeLeft), DEC);
   Serial.print("RPM right: ");
-  Serial.println(getRPM(&ticksRight, &timeRight), DEC);
+  Serial.println(getRPM(&mTicksRight, &mTimeRight), DEC);
+
+  int currentDistance = mFrontUltrasonic.Ranging(CM);
+  Serial.print("Distance: ");
+  Serial.println(currentDistance);
 
   delay(1000); 
 }
@@ -63,16 +74,16 @@ void loop() {
 void setupHMC5883L(){
   //Setup the HMC5883L, and check for errors
   int error; 
-  error = compass.SetScale(1.3); //Set the scale of the compass.
-  if(error != 0) Serial.println(compass.GetErrorText(error)); //check if there is an error, and print if so
+  error = mCompass.SetScale(1.3); //Set the scale of the compass.
+  if(error != 0) Serial.println(mCompass.GetErrorText(error)); //check if there is an error, and print if so
 
-  error = compass.SetMeasurementMode(Measurement_Continuous); // Set the measurement mode to Continuous
-  if(error != 0) Serial.println(compass.GetErrorText(error)); //check if there is an error, and print if so
+  error = mCompass.SetMeasurementMode(Measurement_Continuous); // Set the measurement mode to Continuous
+  if(error != 0) Serial.println(mCompass.GetErrorText(error)); //check if there is an error, and print if so
 }
 
 float getHeading(){
   //Get the reading from the HMC5883L and calculate the heading
-  MagnetometerScaled scaled = compass.ReadScaledAxis(); //scaled values from compass.
+  MagnetometerScaled scaled = mCompass.ReadScaledAxis(); //scaled values from compass.
   float heading = atan2(scaled.YAxis, scaled.XAxis);
 
   // Correct for when signs are reversed.
@@ -93,13 +104,15 @@ int getRPM(volatile int *ticks, long *time){
 
 //Capture The IR Break-Beam Interrupt
 void diskInterruptLeft(){
-  ticksLeft++;
+  mTicksLeft++;
 }
 
 //Capture The IR Break-Beam Interrupt
 void diskInterruptRight(){
-  ticksRight++;
+  mTicksRight++;
 }
+
+
 
 
 
